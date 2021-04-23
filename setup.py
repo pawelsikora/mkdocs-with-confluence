fa283ffaff0ab1002c0ba81461c0ed3844f9e75b
Pawel Sikora <pawel.sikora@nokia.com>
2021-04-23 20:04:21 +0200

    Update README, LICENSE, and yaml workflow files

--- setup.py
+++ setup.py
@@ -1,23 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mkdocs-with-confluence',
-    version='0.1.0',
-    description='MkDocs plugin for uploading markdown documentation to Confluence via Confluence REST API',
-    keywords='mkdocs markdown confluence documentation rest python',
-    url='https://github.com/pawelsikora/mkdocs-with-confluence/',
-    author='Pawel Sikora',
-    author_email='sikor6@gmail.com',
-    license='MIT',
-    python_requires='>=3.6',
-    install_requires=[
-        'mkdocs>=1.1',
-        'jinja2'
-    ],
+    name="mkdocs-with-confluence",
+    version="0.1.1",
+    description="MkDocs plugin for uploading markdown documentation to Confluence via Confluence REST API",
+    keywords="mkdocs markdown confluence documentation rest python",
+    url="https://github.com/pawelsikora/mkdocs-with-confluence/",
+    author="Pawel Sikora",
+    author_email="sikor6@gmail.com",
+    license="MIT",
+    python_requires=">=3.6",
+    install_requires=["mkdocs>=1.1", "jinja2"],
     packages=find_packages(),
-    entry_points={
-        'mkdocs.plugins': [
-            'mkdocs-with-confluence = mkdocs_with_confluence.plugin:MkdocsWithConfluence'
-        ]
-    }
+    entry_points={"mkdocs.plugins": ["mkdocs-with-confluence = mkdocs_with_confluence.plugin:MkdocsWithConfluence"]},
 )
